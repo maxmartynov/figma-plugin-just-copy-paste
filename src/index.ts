e6a41965790992cc94659865326b7a15f5ced816
Max Martynov <martynov.max.s@gmail.com>
2020-04-14 18:49:29 +0300

    Update the calculation function to place the copied items to the right coordinates next to the selected item (not inside)

--- src/index.ts
+++ src/index.ts
@@ -1,141 +1,149 @@
-console.log('figma-plugin-smart-copy-paste: start')
+const selectedItems: ReadonlyArray<SceneNode> = figma.currentPage.selection
+const root: DocumentNode = figma.root
 
-let selectedItems = figma.currentPage.selection
-let root = figma.root
+main()
 
-if (selectedItems.length === 0) {
-  figma.closePlugin('No object selected.')
-}
 
-if (figma.command === 'copy') {
-  smartCopy()
-}
 
-if (figma.command === 'paste') {
-  smartPaste()
+function main (): void {
+  if (selectedItems.length === 0) {
+    return figma.closePlugin('No layer selected')
+  }
+
+  switch (figma.command as string) {
+    case 'copy': {
+      return smartCopy()
+    }
+    case 'paste': {
+      return smartPaste()
+    }
+  }
+
+  figma.closePlugin('Smart Copy Plugin Exit')
 }
 
 function smartCopy () {
-  const ids = selectedItems.map((item: any) => item.id)
-  root.setPluginData('clipboardAction', 'copy')
-  root.setPluginData('clipboardItemsIds', ids.join('|'))
+  const ids: string[] = selectedItems.map((item: SceneNode) => item.id)
+  root.setPluginData('clipboardAction', 'copy' as string)
+  root.setPluginData('clipboardItemsIds', ids.join('|') as string)
 
   figma.closePlugin('Copied')
 }
 
-function smartPaste () {
-  const ids = root.getPluginData('clipboardItemsIds').split('|')
-
-  const selectedIds = selectedItems.map((item: any) => item.id)
-  let selectedItem: any = selectedItems.slice()[0]
-  const pastedItems = []
-
-  const notification = figma.notify('Processing...', {timeout: 32})
+function smartPaste (): void {
+  const ids: string[] = root.getPluginData('clipboardItemsIds').split('|')
+  const selectedIds: string[] = selectedItems.map((item: SceneNode) => item.id)
+  const selectedItem: SceneNode|undefined = selectedItems.slice()[0]
+  const pastedItems: SceneNode|BaseNode[] = []
+  const notification: NotificationHandler = figma
+    .notify('Processing...', {timeout: 32})
 
   for (let id of ids) {
-    const clipboardItem: any = figma.getNodeById(id)
+    const clipboardItem: BaseNode = figma.getNodeById(id)
 
     if (!clipboardItem) {
       figma.notify(`ERROR: Cannot find item to copy: '${id}'`)
     }
 
-    let parent = selectedItem
+    let target: BaseNode|undefined = selectedIds.indexOf(clipboardItem.id) > -1
+      ? undefined
+      : selectedItem
 
-    if (selectedIds.indexOf(clipboardItem.id) > -1) {
-      parent = undefined
-    }
+    target = target ? figma.getNodeById(target.id) : clipboardItem.parent
 
-    const newParent: any = parent
-      ? figma.getNodeById(parent.id)
-      : clipboardItem.parent
+    const pastedItem: SceneNode|BaseNode|undefined = _pasteItem(
+      clipboardItem,
+      target
+    )
 
-    pastedItems.push(_pasteItem(clipboardItem, newParent))
+    pastedItem && pastedItems.push(pastedItem)
   }
 
-  figma.currentPage.selection = pastedItems
+  figma.currentPage.selection = pastedItems as SceneNode[]
 
   setTimeout(() => notification.cancel(), 1000)
   figma.closePlugin('Pasted')
 }
 
-function _pasteItem (item, parent) {
+function _pasteItem (
+  item: SceneNode|BaseNode,
+  target: SceneNode|BaseNode): SceneNode|BaseNode|undefined {
+
   if (!item) {
-    console.error('Could not paste item. Item is undefined.')
+    figma.notify('ERROR: Could not paste item. Item is undefined.')
     return
   }
-  if (!parent) {
-    console.error('Could not paste item. Parent is undefined.')
+  if (!target) {
+    figma.notify('ERROR: Could not paste item. Target is undefined.')
     return
   }
 
-  const _parent = typeof parent.appendChild === 'function'
-    ? parent
-    : parent.parent
+  const point: Point = getPositionRelativeToNode(item, target)
+  let index: number = target.parent.children
+    .findIndex((itm: SceneNode|BaseNode) => itm.id === target.id) + 1
 
-  const newItem: any = item.clone()
-  const point: Point = getPositionRelativeToNode(item, _parent)
+  if (index < 0 || index > target.parent.children.length) {
+    index = target.parent.children.length
+  }
 
-  _parent.appendChild(newItem)
+  const newItem: SceneNode = (item as SceneNode).clone()
+  target.parent.insertChild(index, newItem)
   newItem.x = point.x
   newItem.y = point.y
 
   return newItem
 }
 
-function getPositionRelativeToNode (source, target): Point {
-  let _source = source
-  let _target = target
+function getParentsList (
+  node: SceneNode|BaseNode): Map<string, SceneNode|BaseNode> {
 
-  const isInParent = checkIfNodeIsInParent(_source, _target)
+  const map: Map<string, SceneNode|BaseNode> = new Map()
 
-  if (!isInParent) {
-    _source = target
-    _target = source
+  if (!node) return map
+
+  let _parent: SceneNode|BaseNode|undefined = node.parent
+
+  while (_parent) {
+    map.set(_parent.id, _parent)
+    _parent = _parent.id === root.id ? undefined : _parent.parent
   }
 
-  let point: Point = {x: _source.x, y: _source.y}
-  let _parent = _source.parent
+  return map
+}
 
-  if (isInParent) {
-    while (_parent) {
-      const isNotLast = _target.id !== _parent.id
-      if (isNotLast) {
-        point.x += _parent.x || 0
-        point.y += _parent.y || 0
-      }
-      _parent = isNotLast ? _parent.parent : undefined
+function getPositionRelativeToNode (
+  source: SceneNode|BaseNode,
+  target: SceneNode|BaseNode
+  ): Point {
+
+  const parents1: Map<string, SceneNode|BaseNode> = getParentsList(source)
+  const parents2: Map<string, SceneNode|BaseNode> = getParentsList(target)
+
+  let point: Point = {
+    x: (source as SceneNode).x,
+    y: (source as SceneNode).y
+  }
+  let commonParent: any
+
+  for (let [id, parent] of parents1 as Map<string, any>) {
+    if (parents2.get(id)) {
+      commonParent = parent
+      break
     }
-  } else {
-    point.x *= -1
-    point.y *= -1
+    point.x += parent.x || 0
+    point.y += parent.y || 0
+  }
 
-    while (_parent) {
-      const isNotLast = _target.parent.id !== _parent.id
+  const keys: string[] = Array.from(parents2.keys())
+  let i: number = keys.indexOf(commonParent.id) - 1
 
-      if (isNotLast) {
-        point.x -= _parent.x || 0
-        point.y -= _parent.y || 0
-      }
-      _parent = isNotLast ? _parent.parent : undefined
-    }
+  for (;i >= 0; i--) {
+    const id: string = keys[i]
+    const parent: SceneNode|BaseNode = parents2.get(id) as SceneNode
 
-    point.x += _target.x || 0
-    point.y += _target.y || 0
+    point.x -= parent.x || 0
+    point.y -= parent.y || 0
   }
 
   return point
 }
-
-function checkIfNodeIsInParent (child, parent) {
-  const targetParent = parent.children ? parent : parent.parent
-  let _parent = child.parent
-
-  while (_parent) {
-    if (targetParent.id === _parent.id) return true
-    _parent = _parent.parent
-  }
-
-  return false
-}
-
-figma.closePlugin('Smart Copy Plugin Exit')
